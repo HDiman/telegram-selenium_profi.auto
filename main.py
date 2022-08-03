d0b99a1791f27420d1e005aba7ecbb4cce9b95c5
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-08-03 22:50:16 +0300

    Ready for to move to server

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ def validate_pin(pin):
     else:
         return False
 
+def validate_time(time_enter):
+    if re.search('^\d{1,2}$', time_enter):
+        return True
+    else:
+        return False
+
 
 def telegram_bot(token):
     bot = telebot.TeleBot(token)
@@ -26,35 +32,39 @@ def telegram_bot(token):
 
     @bot.message_handler(content_types=['text'])
     def send_text(message):
+
+        # Function for looping num times
+        def looping(num):
+            for i in range(num):
+                try:
+                    bot.send_message(message.chat.id, "-----------------------------------------------------------")
+                    click_open()
+                    time.sleep(3)
+                    bot.send_message(message.chat.id, "Список заказов:")
+                    all_list = check_all()
+                    if all_list == []:
+                        bot.send_message(message.chat.id, "Заказов нет")
+                    else:
+                        for item in all_list:
+                            bot.send_message(message.chat.id, f"{item}")
+                            print(item)
+                    bot.send_message(message.chat.id, f"Осталось {num - (i + 1)} мин. ...")
+                    time.sleep(57)
+                except Exception as ex:
+                    print(ex)
+                    bot.send_message(message.chat.id, "Error in loop!")
+            bot.send_message(message.chat.id, "... Ожидаю след. действия ... ")
+
         try:
-            if validate_pin(message.text.lower()):
+            if validate_pin(message.text):
                 bot.send_message(message.chat.id, "Код принят")
                 enter_code(message.text)
                 enter_chat()
-                bot.send_message(message.chat.id, "Можно работать")
-            elif message.text == "loop":
-                while True:
-                    try:
-                        bot.send_message(message.chat.id, "Открываем окно ...")
-                        click_open()
-                        time.sleep(3)
-                        bot.send_message(message.chat.id, "Список заказов в окне:")
-                        all_list = check_all()
-                        if all_list == []:
-                            bot.send_message(message.chat.id, "Заказов нет")
-                        else:
-                            for item in all_list:
-                                bot.send_message(message.chat.id, f"{item}")
-                                print(item)
-                        time.sleep(57)
-                    except Exception as ex:
-                        print(ex)
-                        bot.send_message(message.chat.id, "Error in loop!")
-            elif message.text == "stop":
-                bot.send_message(message.chat.id, "... Ожидаем указа ... ")
-                while True:
-                    time.sleep(60)
-                    pass
+                bot.send_message(message.chat.id, "Введите время в минутах")
+            elif validate_time(message.text):
+                bot.send_message(message.chat.id, f"Время установлено на: {message.text} минут")
+                looping(int(message.text))
+
             else:
                 bot.send_message(message.chat.id, "Повторите ввод")
         except Exception as ex:
